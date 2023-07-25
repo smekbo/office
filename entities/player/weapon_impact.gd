1cf4136407116b2cf0f7fd439f8a6d4dc5fd627f
Tegiminis <dragonmaw@gmail.com>
2023-07-25 01:53:46 -0700

    added random rotation to decal added random visibility to dust and sparks

--- entities/player/weapon_impact.gd
+++ entities/player/weapon_impact.gd
@@ -21,13 +21,28 @@ func start(object : PhysicsBody3D, location : Vector3, normal : Vector3 = Vector
 	global_position = location
 	show()
 	animator.play("emit")
+	
 	#set spark and dust practicle directions to the collision normal
 	sparks.process_material.direction = normal
 	dust.process_material.direction = normal
+	
+	# randomize if dust/sparks are visible
+	if randf() > 0.5: 
+		sparks.visible = false
+	if randf() > 0.5: 
+		dust.visible = false
+	
 	# apply hit decal
 	if DECAL:
+		# instance decal and set up other variables
 		var new_decal : Decal = DECAL.instantiate()
 		var decal_normal : Vector3 = Vector3.DOWN + normal
+		var random_rot : int = randi_range(-180, 180)
+		
+		# parent, place, and properly rotate decal
 		object.add_child(new_decal)
 		new_decal.global_position = location
 		new_decal.global_rotation = decal_normal
+		
+		# add a little random rotation to spice it up
+		new_decal.global_transform.basis = new_decal.global_transform.basis.rotated(normal, deg_to_rad(random_rot))
