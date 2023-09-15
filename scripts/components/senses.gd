6645f0be2d412d475aa0f670afd976207c07f5ed
Tegiminis <dragonmaw@gmail.com>
2023-09-14 23:51:40 -0700

    random fixup

--- scripts/components/senses.gd
+++ scripts/components/senses.gd
@@ -11,27 +11,29 @@ func _ready():
 	pass
 	
 func heard_sound(location: Vector3):
+	# i heard something!
 	emit_signal("heard", location)
 
 func _on_hearing_area_entered(area):
+	# connect sound to hearing when entering area
 	area.connect("sound_made", heard_sound)
-	pass # Replace with function body.
 
 func _on_hearing_area_exited(area):
+	# disconnect sound from hearing when exiting area
 	area.disconnect("sound_made", heard_sound)
-	pass # Replace with function body.
 
 func _physics_process(delta):
-	# 
+	# does all the vision processing
+	# start with overlapping bodies (anything in layer 1)
 	var overlapping = vision_area.get_overlapping_bodies()
 	if overlapping:
-		# print_debug("Debug: Player is in vision cone")
+		# player will always be the only entry in overlapping (for now...)
 		var player = overlapping[0]
+		# raycast to player position to see if they are visible
 		vision_ray.target_position = vision_ray.to_local(player.global_transform.origin)
-		print_debug(str("Debug: Player is ", player))
 		var collided : Object = vision_ray.get_collider()
 		if collided:
 			var is_player = collided.get_collision_layer_value(1)
-			print_debug("Debug: Player is visible")
+			# if the raycast hits the player, send the "saw something" signal
 			if is_player:
 				emit_signal("saw", collided)
