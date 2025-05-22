1507a24bab34594eca4be1676718bf1a0b0be36c
Tegiminis <dragonmaw@gmail.com>
2025-05-21 22:30:06 -0700

    added ammo splitting (ammo pickups split evenly between items)

--- pickups/pickup_ammo/pickup_ammo.gd
+++ pickups/pickup_ammo/pickup_ammo.gd
@@ -5,9 +5,13 @@ extends Pickup
 
 func _on_body_entered(body: Node3D) -> void:
 	var _inventory : Array = body.get("weapon_inventory")
+	var _weapons : Array = []
 	var _used : bool = false
 	
 	for item : Weapon in _inventory:
 		if item.ammo_type == ammo_type:
-			_used = item.add_reserve(ammo_amount)
+			_weapons.append(item)
+	var ammo_split = ammo_amount / _weapons.size()
+	for item in _weapons:
+		_used = item.add_reserve(ammo_split)
 	if _used: self.queue_free()
