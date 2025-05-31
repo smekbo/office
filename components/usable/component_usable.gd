24d13976c337b2e6c901b99b0b9e474e4e2a63de
Tegiminis <dragonmaw@gmail.com>
2025-05-30 20:02:20 -0700

    added warnings library stub added sound effect to health usable (the watercooler)

--- components/usable/component_usable.gd
+++ components/usable/component_usable.gd
@@ -6,9 +6,16 @@ class_name ComponentUsable
 ## How long you have to wait between uses.
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var cooldown : float = 1
 var cooldown_timer : float = 0
+@export var use_sound : AudioStream = null
+@onready var sfx_player : AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 ## This object was used. Should be linked to a function on the parent object that does an action when used.
 signal used(user)
+
+func _ready() -> void:
+	if use_sound: 
+		sfx_player.set_stream(use_sound)
+		Warnings.placeholder_warning(owner, use_sound)
 
 func _physics_process(delta: float) -> void:
 	cooldown_timer = max(0, cooldown_timer - delta)
@@ -17,4 +24,5 @@ func _physics_process(delta: float) -> void:
 func use(user = null):
 	if cooldown_timer <= 0:
 		used.emit(user)
+		sfx_player.play()
 		cooldown_timer = cooldown
---
+++ libraries/lib_warnings.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Warnings
+
+static func placeholder_warning(object = null, resource = null):
+	if resource.resource_path.contains("debug"): 
+			push_warning("This is a debug/placeholder resource and should be replaced: " + resource.resource_path + " on " + str(object))
